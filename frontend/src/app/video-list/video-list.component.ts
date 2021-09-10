e58a72a255b46c8643d2302f1a6705f62f50e1f7
Anders Payerl <6882326+APayerl@users.noreply.github.com>
2021-09-10 23:49:06 +0200

    master - add search and filter ability

--- frontend/src/app/video-list/video-list.component.ts
+++ frontend/src/app/video-list/video-list.component.ts
@@ -11,10 +11,19 @@ export class VideoListComponent implements OnInit {
     constructor(public videoService: VideoService) { }
 
     videos: Video[] = [];
+    filter: any = (tx: Video) => tx.title.startsWith("");
+    get filteredVideos(): Video[] {
+        return this.videos.filter(this.filter);
+    }
 
     ngOnInit(): void {
         this.videoService.getVideos().then(val => {
             this.videos = val.sort((vid1, vid2) => vid1.title.toUpperCase() > vid2.title.toUpperCase() ? 1 : -1);
         })
+    }
+
+    searchVideo(event: any) {
+        console.log(event.target.value);
+        this.filter = (tx: Video) => tx.title.startsWith(event.target.value);
     }
 }
